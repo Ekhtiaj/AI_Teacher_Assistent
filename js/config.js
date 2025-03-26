5051809af26dc97d395e17e41b6f827814e2a70b
Md Ekhtiaj Hasan <ekhtiajhasan17@gmail.com>
2025-03-26 21:19:03 +0800

    with a new api key

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const API_KEY = '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';
+const API_KEY = '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';
 const API_URL = "https://openrouter.ai/api/v1/chat/completions";
 const MODEL = "deepseek/deepseek-r1-distill-llama-70b:free";
